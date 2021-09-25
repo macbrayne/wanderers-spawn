534208753c273495b703eb79641d7d1d321571d6
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-09-26 00:20:22 +0200

    rework distance and time conditions rework PlayerTickEvent in general

--- common/src/main/java/de/macbrayne/architectury/wanderers_spawn/config/ToggledValue.java
+++ common/src/main/java/de/macbrayne/architectury/wanderers_spawn/config/ToggledValue.java
@@ -3,7 +3,7 @@ package de.macbrayne.architectury.wanderers_spawn.config;
 import java.util.function.Predicate;
 
 public class ToggledValue<T> {
-    private boolean enabled = true;
+    private boolean enabled = false;
     private T value;
 
     private ToggledValue() {
@@ -26,11 +26,16 @@ public class ToggledValue<T> {
         return value;
     }
 
+    public void setAndEnable(T value) {
+        enabled = true;
+        this.value = value;
+    }
+
     public void setValue(T value) {
         this.value = value;
     }
 
-    public boolean ifEnabled(Predicate<T> action) {
-        return enabled && action.test(value);
+    public boolean notEnabledOr(Predicate<T> action) {
+        return !enabled || action.test(value);
     }
 }
--- common/src/main/java/de/macbrayne/architectury/wanderers_spawn/events/PlayerTickEvent.java
+++ common/src/main/java/de/macbrayne/architectury/wanderers_spawn/events/PlayerTickEvent.java
@@ -12,50 +12,49 @@ import net.minecraft.world.phys.Vec3;
 
 public class PlayerTickEvent {
     private static final PlayerConfig MOD_CONFIG = Reference.globalConfig;
-    private static int distanceWalkedRawValue = 0;
-    private static int timeLeft = MOD_CONFIG.timeSpentCondition.getValue();
-    private static int distanceLeft = MOD_CONFIG.distanceWalkedCondition.getValue();
+    private static boolean distanceTriggered = false;
+    private static boolean timeSpentTriggered = false;
 
     public static void tick(ServerPlayer player) {
         if (player.isAlive()) {
-            if(MOD_CONFIG.timeSpentCondition.isEnabled() && timeLeft > 0) {
-                timeLeft -= 1;
-            }
-            if(MOD_CONFIG.distanceWalkedCondition.isEnabled() && distanceLeft > 0) {
-                int oldValue = distanceWalkedRawValue;
-                distanceWalkedRawValue = player.getStats().getValue(Stats.CUSTOM.get(Stats.WALK_ONE_CM)) +
-                        player.getStats().getValue(Stats.CUSTOM.get(Stats.SPRINT_ONE_CM));
+            int distanceWalkedRawValue = player.getStats().getValue(Stats.CUSTOM.get(Stats.WALK_ONE_CM)) +
+                    player.getStats().getValue(Stats.CUSTOM.get(Stats.SPRINT_ONE_CM));
 
-                if(distanceWalkedRawValue != 0) {
-                    distanceWalkedRawValue /= 100; // Convert from CM into Metres
-                }
+            if (!distanceTriggered && MOD_CONFIG.timeSpentCondition.notEnabledOr(time -> player.tickCount % time == 0)) {
+                distanceTriggered = true;
+            }
+            if (!timeSpentTriggered && MOD_CONFIG.distanceWalkedCondition.notEnabledOr(distance -> distanceWalkedRawValue % distance == 0)) {
+                timeSpentTriggered = true;
+            }
 
-                distanceLeft -= Math.min(0, distanceWalkedRawValue - oldValue);
-            }
-            if (MOD_CONFIG.directSunlightCondition.ifEnabled(ignored -> !hasDirectSunlight(player)) ||
-                    MOD_CONFIG.afterCondition.ifEnabled(after -> player.level.dayTime() < after) ||
-                    MOD_CONFIG.beforeCondition.ifEnabled(before -> player.level.dayTime() > before) ||
-                    MOD_CONFIG.minHealthCondition.ifEnabled(minHealth -> player.getHealth() < minHealth) ||
-                    MOD_CONFIG.noMonstersNearbyCondition.ifEnabled(ignored -> areMonstersNearby(player))) {
-                return;
-            }
-            if(timeLeft <= 0 && distanceLeft <= 0) {
+            if (MOD_CONFIG.directSunlightCondition.notEnabledOr(ignored -> hasDirectSunlight(player)) &&
+                    timeSpentTriggered &&
+                    distanceTriggered &&
+                    MOD_CONFIG.afterCondition.notEnabledOr(after -> player.level.dayTime() > after) &&
+                    MOD_CONFIG.beforeCondition.notEnabledOr(before -> player.level.dayTime() < before) &&
+                    MOD_CONFIG.minHealthCondition.notEnabledOr(minHealth -> player.getHealth() > minHealth) &&
+                    MOD_CONFIG.noMonstersNearbyCondition.notEnabledOr(ignored -> !areMonstersNearby(player))) {
                 reset(player);
             }
         }
     }
 
     private static boolean hasDirectSunlight(ServerPlayer player) {
-        return player.level.getHeight(Heightmap.Types.WORLD_SURFACE, (int) player.position().x, (int) player.position().z) >= player.position().y;
+        return player.position().y >= player.level.getHeight(Heightmap.Types.WORLD_SURFACE, (int) player.position().x, (int) player.position().z);
     }
 
     private static boolean areMonstersNearby(ServerPlayer player) {
         Vec3 blockCentre = Vec3.atBottomCenterOf(player.blockPosition());
-        return player.level.getEntitiesOfClass(Monster.class, new AABB(blockCentre.x() - 8.0D, blockCentre.y() - 5.0D, blockCentre.z() - 8.0D, blockCentre.x() + 8.0D, blockCentre.y() + 5.0D, blockCentre.z() + 8.0D), (monster) -> monster.isPreventingPlayerRest(player)).isEmpty();
+        return !player.level.getEntitiesOfClass(Monster.class,
+                        new AABB(blockCentre.x() - 8.0D, blockCentre.y() - 5.0D,blockCentre.z() - 8.0D,
+                                blockCentre.x() + 8.0D, blockCentre.y() + 5.0D, blockCentre.z() + 8.0D),
+                        (monster) -> monster.isPreventingPlayerRest(player))
+                .isEmpty();
     }
 
     private static void reset(ServerPlayer player) {
         player.setRespawnPosition(player.level.dimension(), new BlockPos(player.position()), 0, false, true);
-        timeLeft = MOD_CONFIG.timeSpentCondition.getValue();
+        distanceTriggered = false;
+        timeSpentTriggered = false;
     }
 }
